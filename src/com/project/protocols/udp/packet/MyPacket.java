8bc377ddced30f7c531a9b6eedc8efebded561ea
Artilius <haydayclonee@gmail.com>
2019-11-15 23:22:57 +0700

    Define UDP packet with header to implement TCP

--- src/com/project/protocols/udp/packet/MyPacket.java
+++ src/com/project/protocols/udp/packet/MyPacket.java
@@ -1,80 +1,74 @@
 package com.project.protocols.udp.packet;
 
-import java.io.Serializable;
-import java.net.InetAddress;
+import com.project.protocols.Constant;
+import java.util.Arrays;
 import java.nio.ByteBuffer;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-public class MyPacket implements Serializable{
-    private static final int MAX_SIZE = 1012;
-    private static final int HEADER_SIZE = 12;
-    // header info
+public class MyPacket{
+    // header info 12 bytes
+    // type 0 : metaData
+    //      1 : Data
+    //      2 : ack
     private int type;
+    private int checkSum;
     private int seqNum;
-    private int packetSize;
+
     // data of packet
     private byte[] packetData;
+    private long fileSize = 0;
 
-    MyPacket(int type, int size, int seqNum, byte[] data) throws Exception {
-        if (size > MAX_SIZE) {
-            throw new Exception("Excess packet max size (1012 bytes)");
-        }
-        this.type = type;
-        this.packetSize = size;
-        this.seqNum = seqNum;
+    // Init 1
+    public MyPacket(int sequenceNumber, byte[] data, int type) {
+        this.seqNum = sequenceNumber;
         this.packetData = data;
+        checkSum = -sequenceNumber;
+        for (byte b : data) {
+            checkSum -= b;
+        }
     }
-    public static MyPacket createAck(int sequence, int type) throws Exception{
-        return new MyPacket(type, HEADER_SIZE, sequence, new byte[0]);
+
+    // Init 2
+    public MyPacket(DatagramPacket packet, int type) {
+        byte[] data = packet.getData();
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        checkSum = buffer.getInt();
+        seqNum = buffer.getInt();
+        this.packetData = Arrays.copyOfRange(buffer.array(), Constant.PACKET_HEADER_SIZE, data.length);
+        if (type == 0 && seqNum == 0 && isValid()) {
+            fileSize = buffer.getLong();
+        }
     }
+
+    // Sum checker
+    public boolean isValid() {
+        int sum = checkSum;
+        sum += seqNum;
+        for (byte b : packetData) {
+            sum += b;
+        }
+        return sum == 0;
+    }
+
     public int getSeqNum() {
         return seqNum;
-    }
-
-    public long getPacketSize() {
-        return packetSize;
     }
 
     public byte[] getPacketData() {
         return packetData;
     }
 
-    public int getType(){
-        return type;
-    }
-
     public byte[] toBytes() {
-        ByteBuffer buffer = ByteBuffer.allocate(packetSize);
-        buffer.putInt(type);
-        buffer.putInt(packetSize);
+        ByteBuffer buffer = ByteBuffer.allocate(packetData.length + Constant.PACKET_HEADER_SIZE);
+        buffer.putInt(checkSum);
         buffer.putInt(seqNum);
-        buffer.put(packetData, 0, packetSize - HEADER_SIZE);
+        buffer.put(packetData, 0, packetData.length);
         return buffer.array();
     }
 
-    public static MyPacket createPacket(byte[] bytes) throws Exception {
-        ByteBuffer buffer = ByteBuffer.wrap(bytes);
-        int type = buffer.getInt();
-        int length = buffer.getInt();
-        int seqNum = buffer.getInt();
-        if (length > HEADER_SIZE) {
-            byte[] data = new byte[length - HEADER_SIZE];
-            buffer.get(data, 0, length - HEADER_SIZE);
-            return new MyPacket(type, length, seqNum, data);
-        } else {
-            return new MyPacket(type, length, seqNum, new byte[0]);
-        }
+    public int length() {
+        return packetData.length + Constant.PACKET_HEADER_SIZE;
     }
-
-    public void send(String address, int port, DatagramSocket socket) {
-        try {
-            DatagramPacket sendPacket = new DatagramPacket(this.toBytes(),
-                    this.packetSize, InetAddress.getByName(address), port);
-            socket.send(sendPacket);
-        } catch (Exception e) {
-            System.out.println("Exception when sending packet");
-        }
-    }
-
 }
